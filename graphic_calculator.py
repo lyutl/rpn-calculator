5805801957721ed4f399571a1c09f5a35b86baa3
froschalad <90521030+froschalad@users.noreply.github.com>
2022-10-22 21:23:06 +0300

    Update graphic_calculator.py

--- graphic_calculator.py
+++ graphic_calculator.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     RPN = TransformExpression(stack_test)
 
 
-def btn_click(item):
+def bt_click(item):
     """
     docstring
     """
@@ -59,7 +59,7 @@ input_text = StringVar()
 input_frame = Frame(win, width=312, height=50, bd=0, highlightbackground="black", \
                     highlightcolor="black", highlightthickness=2)
 input_frame.pack(side=TOP)
-input_field = Entry(input_frame,font=('arial',18,'bold'),textvariable=input_text,width=50,bg="#eee", \
+input_field=Entry(input_frame,font=('arial',18,'bold'),textvariable=input_text,width=50,bg="#eee", \
                     bd=0, justify=RIGHT)
 input_field.grid(row=0, column=0)
 input_field.pack(ipady=10)
@@ -70,58 +70,60 @@ CLEAR = Button(btns_frame,text="C",fg="black", width=10, height=3, bd=0, bg="#ee
                cursor="hand2", command=lambda: bt_clear()).grid(row=0, column=0, padx=1, pady=1)
 CLEAR.pack()
 LEFT_BRACKET = Button(btns_frame,text="(",fg="black", width=10, height=3, bd=0, bg="#eee", \
-                      cursor="hand2",command=lambda:btn_click('(')).grid(row=0, column=1, padx=1, pady=1)
+                      cursor="hand2",\
+                      command=lambda:bt_click('(')).grid(row=0,column=1,padx=1,pady=1)
 LEFT_BRACKET.pack()
 RIGHT_BRACKET = Button(btns_frame,text=")",fg="black", width=10, height=3, bd=0, bg="#eee", \
-                       cursor="hand2",command=lambda:btn_click(')')).grid(row=0,column=2,padx=1,pady=1)
+                       cursor="hand2",command=lambda:bt_click(')')).grid(row=0,column=2,padx=1,pady=1)
 RIGHT_BRACKET.pack()
 DIVIDE = Button(btns_frame,text="/",fg="black", width=10, height=3, bd=0, bg="#eee", \
-                cursor="hand2",command=lambda:btn_click("/")).grid(row=0, column=3, padx=1, pady=1)
+                cursor="hand2",command=lambda:bt_click("/")).grid(row=0, column=3, padx=1, pady=1)
 DIVIDE.pack()
 SEVEN = Button(btns_frame,text="7",fg="black", width=10,height=3,bd=0, bg="#fff", cursor="hand2", \
-               command=lambda: btn_click(7)).grid(row=1, column=0, padx=1, pady=1)
+               command=lambda: bt_click(7)).grid(row=1, column=0, padx=1, pady=1)
 SEVEN.pack()
 EIGHT = Button(btns_frame,text="8",fg="black", width=10,height=3,bd=0, bg="#fff", cursor="hand2", \
-               command=lambda: btn_click(8)).grid(row=1, column=1, padx=1, pady=1)
+               command=lambda: bt_click(8)).grid(row=1, column=1, padx=1, pady=1)
 EIGHT.pack()
 NINE = Button(btns_frame,text="9",fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2", \
-              command=lambda: btn_click(9)).grid(row=1, column=2, padx=1, pady=1)
+              command=lambda: bt_click(9)).grid(row=1, column=2, padx=1, pady=1)
 NINE.pack()
 MULTIPLY = Button(btns_frame,text="*",fg="black",width=10,height=3,bd=0,bg="#eee",cursor="hand2",\
-                  command=lambda: btn_click("*")).grid(row=1, column=3, padx=1, pady=1)
+                  command=lambda: bt_click("*")).grid(row=1, column=3, padx=1, pady=1)
 MULTIPLY.pack()
 FOUR = Button(btns_frame,text="4",fg="black",width=10, height=3, bd=0, bg="#fff", cursor="hand2", \
-              command=lambda: btn_click(4)).grid(row=2, column=0, padx=1, pady=1)
+              command=lambda: bt_click(4)).grid(row=2, column=0, padx=1, pady=1)
 FOUR.pack()
 FIVE = Button(btns_frame,text="5",fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2", \
-              command=lambda: btn_click(5)).grid(row=2, column=1, padx=1, pady=1)
+              command=lambda: bt_click(5)).grid(row=2, column=1, padx=1, pady=1)
 FIVE.pack()
 SIX = Button(btns_frame,text="6",fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2", \
              command=lambda: btn_click(6)).grid(row=2, column=2, padx=1, pady=1)
 SIX.pack()
 MINUS = Button(btns_frame,text="-",fg="black",width=10,height=3, bd=0, bg="#eee", cursor="hand2", \
-               command=lambda: btn_click("-")).grid(row=2, column=3, padx=1, pady=1)
+               command=lambda: bt_click("-")).grid(row=2, column=3, padx=1, pady=1)
 MINUS.pack()
 ONE = Button(btns_frame,text="1",fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2", \
-             command=lambda: btn_click(1)).grid(row=3, column=0, padx=1, pady=1)
+             command=lambda: bt_click(1)).grid(row=3, column=0, padx=1, pady=1)
 ONE.pack()
 TWO = Button(btns_frame,text="2",fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2", \
-             command=lambda: btn_click(2)).grid(row=3, column=1, padx=1, pady=1)
+             command=lambda: bt_click(2)).grid(row=3, column=1, padx=1, pady=1)
 TWO.pack()
 THREE = Button(btns_frame,text="3",fg="black",width=10,height=3,bd=0, bg="#fff", cursor="hand2", \
-               command=lambda: btn_click(3)).grid(row=3, column=2, padx=1, pady=1)
+               command=lambda: bt_click(3)).grid(row=3, column=2, padx=1, pady=1)
 THREE.pack()
 PLUS = Button(btns_frame,text="+",fg="black", width=10, height=3, bd=0, bg="#eee", cursor="hand2", \
-              command=lambda: btn_click("+")).grid(row=3, column=3, padx=1, pady=1)
+              command=lambda: bt_click("+")).grid(row=3, column=3, padx=1, pady=1)
 PLUS.pack()
 ZERO = Button(btns_frame,text="0",fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2", \
-              command=lambda: btn_click(0)).grid(row=4, column=0, padx=1, pady=1)
+              command=lambda: bt_click(0)).grid(row=4, column=0, padx=1, pady=1)
 ZERO.pack()
 EXPONENTIATION = Button(btns_frame,text="x^y",fg="black",width=10,height=3,bd=0,\
-                        bg="#fff",cursor="hand2",command=lambda: btn_click("^")).grid(row=4, column=1, padx=1, pady=1)
+                        bg="#fff",cursor="hand2",\
+                        command=lambda: bt_click("^")).grid(row=4, column=1, padx=1, pady=1)
 EXPONENTIATION.pack()
 POINT = Button(btns_frame,text=".",fg="black",width=10,height=3,bd=0,bg="#eee", cursor="hand2", \
-               command=lambda: btn_click(".")).grid(row=4, column=2, padx=1, pady=1)
+               command=lambda: bt_click(".")).grid(row=4, column=2, padx=1, pady=1)
 POINT.pack()
 EQUALS = Button(btns_frame,text="=",fg="black",width=10,height=3,bd=0,bg="#eee", cursor="hand2", \
                 command=lambda: bt_equal()).grid(row=4, column=3, padx=1, pady=1)

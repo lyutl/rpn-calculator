51b3b6320ef5ff19b2e66ee48044771b268c82b9
froschalad <90521030+froschalad@users.noreply.github.com>
2022-10-22 20:28:10 +0300

    Update graphic_calculator.py

--- graphic_calculator.py
+++ graphic_calculator.py
@@ -49,24 +49,24 @@ def bt_equal():
 
 input_text = StringVar()
 
-input_frame = Frame(win, width=312, height=50, bd=0, highlightbackground="black", highlightcolor="black",
-                    highlightthickness=2)
+input_frame = Frame(win, width=312, height=50, bd=0, highlightbackground="black", 
+                    highlightcolor="black", highlightthickness=2)
 input_frame.pack(side=TOP)
-input_field = Entry(input_frame, font=('arial', 18, 'bold'), textvariable=input_text, width=50, bg="#eee", bd=0,
-                    justify=RIGHT)
+input_field = Entry(input_frame, font=('arial', 18, 'bold'), textvariable=input_text, width=50, bg="#eee", 
+                    bd=0, justify=RIGHT)
 input_field.grid(row=0, column=0)
 input_field.pack(ipady=10)
 btns_frame = Frame(win, width=312, height=272.5, bg="grey")
 btns_frame.pack()
 
-clear = Button(btns_frame, text="C", fg="black", width=10, height=3, bd=0, bg="#eee", cursor="hand2",
-               command=lambda: bt_clear()).grid(row=0, column=0, padx=1, pady=1)
-left_bracket = Button(btns_frame, text="(", fg="black", width=10, height=3, bd=0, bg="#eee", cursor="hand2",
-                      command=lambda: btn_click('(')).grid(row=0, column=1, padx=1, pady=1)
-right_bracket = Button(btns_frame, text=")", fg="black", width=10, height=3, bd=0, bg="#eee", cursor="hand2",
-                       command=lambda: btn_click(')')).grid(row=0, column=2, padx=1, pady=1)
-divide = Button(btns_frame, text="/", fg="black", width=10, height=3, bd=0, bg="#eee", cursor="hand2",
-                command=lambda: btn_click("/")).grid(row=0, column=3, padx=1, pady=1)
+clear = Button(btns_frame, text="C", fg="black", width=10, height=3, bd=0, bg="#eee", 
+               cursor="hand2", command=lambda: bt_clear()).grid(row=0, column=0, padx=1, pady=1)
+left_bracket = Button(btns_frame, text="(", fg="black", width=10, height=3, bd=0, bg="#eee", 
+                      cursor="hand2", command=lambda: btn_click('(')).grid(row=0, column=1, padx=1, pady=1)
+right_bracket = Button(btns_frame, text=")", fg="black", width=10, height=3, bd=0, bg="#eee", 
+                       cursor="hand2", command=lambda: btn_click(')')).grid(row=0, column=2, padx=1, pady=1)
+divide = Button(btns_frame, text="/", fg="black", width=10, height=3, bd=0, bg="#eee", 
+                cursor="hand2", command=lambda: btn_click("/")).grid(row=0, column=3, padx=1, pady=1)
 seven = Button(btns_frame, text="7", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2",
                command=lambda: btn_click(7)).grid(row=1, column=0, padx=1, pady=1)
 eight = Button(btns_frame, text="8", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2",

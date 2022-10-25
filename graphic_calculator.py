d2b17f21dea20ac3608d59f6580c91192c7b6cf5
Lyubov Talanina <72165770+lyutl@users.noreply.github.com>
2022-10-25 21:03:32 +0300

    Add class

--- graphic_calculator.py
+++ graphic_calculator.py
@@ -17,48 +17,48 @@ if __name__ == '__main__':
     RPN = TransformExpression(stack_test)
 
 
-def bt_click(item):
-    """
-    docstring
-    """
-    global EXPRESSION
-    EXPRESSION = EXPRESSION + str(item)
-    unexpected_combos = ['+-', '+*', '+/', '+^', '-+', '-*', '-/', '-^', '*+', '*-',
-                         '*/', '*^', '/+', '/-', '/*', '/^', '^+', '^-', '^*', '^/',
-                         '++', '--', '**', '//', '^^', '+.', '-.', '*.', '/.', '^.',
-                         '.+', '.-', '.*', './', '.^']
-    if EXPRESSION[-2:] in unexpected_combos:
-        EXPRESSION = EXPRESSION[:-1]
-    input_text.set(EXPRESSION)
+class Calculator:
+    def __init__(self):
+        self.expression = ""
+
+    def bt_click(self, item):
+        """
+        Presses a button
+        """
+        self.expression = self.expression + str(item)
+        unexpected_combos = ['+-', '+*', '+/', '+^', '-+', '-*', '-/', '-^', '*+', '*-',
+                             '*/', '*^', '/+', '/-', '/*', '/^', '^+', '^-', '^*', '^/',
+                             '++', '--', '**', '//', '^^', '+.', '-.', '*.', '/.', '^.',
+                             '.+', '.-', '.*', './', '.^']
+        if self.expression[-2:] in unexpected_combos:
+            self.expression = self.expression[:-1]
+        input_text.set(self.expression)
+
+    def bt_clear(self):
+        """
+        Clears input field
+        """
+        self.expression = ""
+        input_text.set("")
+
+    def bt_equal(self):
+        """
+        Outputs the result of expression
+        """
+        self.expression = check_expression(self.expression)
+        rpn_list = RPN.to_list(self.expression)
+        num1 = Solution(stack_test)
+        rpn_str = RPN.transformation(rpn_list)
+        result = num1.display_calculation(num1.to_list(rpn_str))
+        result_str = str(result)
+        if result_str[-2:] == '01':
+            result_str = re.sub('[0]?[0]?[0]?[0]?[0]?[0]?[0]?[0]?[0]?[0]?[0]?[0]?01', '', result_str)
+            result = float(result_str)
+        self.expression = ""
+        input_text.set(result)
 
 
-def bt_clear():
-    """
-    docstring
-    """
-    global EXPRESSION
-    EXPRESSION = ""
-    input_text.set("")
-
-
-def bt_equal():
-    """
-    docstring
-    """
-    global EXPRESSION
-    EXPRESSION = check_expression(EXPRESSION)
-    rpn_list = RPN.to_list(EXPRESSION)
-    num1 = Solution(stack_test)
-    rpn_str = RPN.transformation(rpn_list)
-    result = num1.display_calculation(num1.to_list(rpn_str))
-    result_str = str(result)
-    if result_str[-2:] == '01':
-        result_str = re.sub('[0]?[0]?[0]?[0]?[0]?[0]?[0]?[0]?[0]?[0]?[0]?[0]?01', '', result_str)
-        result = float(result_str)
-    EXPRESSION = ""
-    input_text.set(result)
-
-
+calculator = Calculator()
 input_text = tk.StringVar()
 
 input_frame = tk.Frame(win, width=312, height=50, bd=0, highlightbackground="black",
@@ -72,83 +72,83 @@ btns_frame = tk.Frame(win, width=312, height=272.5, bg="grey")
 btns_frame.pack()
 
 CLEAR = tk.Button(btns_frame, text="C", fg="black", width=10, height=3, bd=0, bg="#eee",
-                  cursor="hand2", command=bt_clear)
+                  cursor="hand2", command=calculator.bt_clear)
 CLEAR.grid(row=0, column=0, padx=1, pady=1)
 LEFT_BRACKET = tk.Button(btns_frame, text="(", fg="black", width=10, height=3, bd=0, bg="#eee",
                          cursor="hand2",
-                         command=lambda: bt_click('('))
+                         command=lambda: calculator.bt_click('('))
 LEFT_BRACKET.grid(row=0, column=1, padx=1, pady=1)
 RIGHT_BRACKET = tk.Button(btns_frame, text=")", fg="black", width=10, height=3, bd=0, bg="#eee",
                           cursor="hand2",
-                          command=lambda: bt_click(')'))
+                          command=lambda: calculator.bt_click(')'))
 RIGHT_BRACKET.grid(row=0, column=2, padx=1, pady=1)
 DIVIDE = tk.Button(btns_frame, text="/", fg="black", width=10, height=3, bd=0, bg="#eee",
                    cursor="hand2",
-                   command=lambda: bt_click("/"))
+                   command=lambda: calculator.bt_click("/"))
 DIVIDE.grid(row=0, column=3, padx=1, pady=1)
 SEVEN = tk.Button(btns_frame, text="7", fg="black", width=10, height=3, bd=0, bg="#fff",
                   cursor="hand2",
-                  command=lambda: bt_click(7))
+                  command=lambda: calculator.bt_click(7))
 SEVEN.grid(row=1, column=0, padx=1, pady=1)
 EIGHT = tk.Button(btns_frame, text="8", fg="black", width=10, height=3, bd=0, bg="#fff",
                   cursor="hand2",
-                  command=lambda: bt_click(8))
+                  command=lambda: calculator.bt_click(8))
 EIGHT.grid(row=1, column=1, padx=1, pady=1)
 NINE = tk.Button(btns_frame, text="9", fg="black", width=10, height=3, bd=0, bg="#fff",
                  cursor="hand2",
-                 command=lambda: bt_click(9))
+                 command=lambda: calculator.bt_click(9))
 NINE.grid(row=1, column=2, padx=1, pady=1)
 MULTIPLY = tk.Button(btns_frame, text="*", fg="black", width=10, height=3, bd=0, bg="#eee",
                      cursor="hand2",
-                     command=lambda: bt_click("*"))
+                     command=lambda: calculator.bt_click("*"))
 MULTIPLY.grid(row=1, column=3, padx=1, pady=1)
 FOUR = tk.Button(btns_frame, text="4", fg="black", width=10, height=3, bd=0, bg="#fff",
                  cursor="hand2",
-                 command=lambda: bt_click(4))
+                 command=lambda: calculator.bt_click(4))
 FOUR.grid(row=2, column=0, padx=1, pady=1)
 FIVE = tk.Button(btns_frame, text="5", fg="black", width=10, height=3, bd=0, bg="#fff",
                  cursor="hand2",
-                 command=lambda: bt_click(5))
+                 command=lambda: calculator.bt_click(5))
 FIVE.grid(row=2, column=1, padx=1, pady=1)
 SIX = tk.Button(btns_frame, text="6", fg="black", width=10, height=3, bd=0, bg="#fff",
                 cursor="hand2",
-                command=lambda: bt_click(6))
+                command=lambda: calculator.bt_click(6))
 SIX.grid(row=2, column=2, padx=1, pady=1)
 MINUS = tk.Button(btns_frame, text="-", fg="black", width=10, height=3, bd=0, bg="#eee",
                   cursor="hand2",
-                  command=lambda: bt_click("-"))
+                  command=lambda: calculator.bt_click("-"))
 MINUS.grid(row=2, column=3, padx=1, pady=1)
 ONE = tk.Button(btns_frame, text="1", fg="black", width=10, height=3, bd=0, bg="#fff",
                 cursor="hand2",
-                command=lambda: bt_click(1))
+                command=lambda: calculator.bt_click(1))
 ONE.grid(row=3, column=0, padx=1, pady=1)
 TWO = tk.Button(btns_frame, text="2", fg="black", width=10, height=3, bd=0, bg="#fff",
                 cursor="hand2",
-                command=lambda: bt_click(2))
+                command=lambda: calculator.bt_click(2))
 TWO.grid(row=3, column=1, padx=1, pady=1)
 THREE = tk.Button(btns_frame, text="3", fg="black", width=10, height=3, bd=0, bg="#fff",
                   cursor="hand2",
-                  command=lambda: bt_click(3))
+                  command=lambda: calculator.bt_click(3))
 THREE.grid(row=3, column=2, padx=1, pady=1)
 PLUS = tk.Button(btns_frame, text="+", fg="black", width=10, height=3, bd=0, bg="#eee",
                  cursor="hand2",
-                 command=lambda: bt_click("+"))
+                 command=lambda: calculator.bt_click("+"))
 PLUS.grid(row=3, column=3, padx=1, pady=1)
 ZERO = tk.Button(btns_frame, text="0", fg="black", width=10, height=3, bd=0, bg="#fff",
                  cursor="hand2",
-                 command=lambda: bt_click(0))
+                 command=lambda: calculator.bt_click(0))
 ZERO.grid(row=4, column=0, padx=1, pady=1)
 EXPONENTIATION = tk.Button(btns_frame, text="x^y", fg="black", width=10, height=3, bd=0,
                            bg="#fff", cursor="hand2",
-                           command=lambda: bt_click("^"))
+                           command=lambda: calculator.bt_click("^"))
 EXPONENTIATION.grid(row=4, column=1, padx=1, pady=1)
 POINT = tk.Button(btns_frame, text=".", fg="black", width=10, height=3, bd=0, bg="#eee",
                   cursor="hand2",
-                  command=lambda: bt_click("."))
+                  command=lambda: calculator.bt_click("."))
 POINT.grid(row=4, column=2, padx=1, pady=1)
 EQUALS = tk.Button(btns_frame, text="=", fg="black", width=10, height=3, bd=0, bg="#eee",
                    cursor="hand2",
-                   command=bt_equal)
+                   command=calculator.bt_equal)
 EQUALS.grid(row=4, column=3, padx=1, pady=1)
 
 win.mainloop()
